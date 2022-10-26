a2129e94c82e7ca0fb41e7006c97dd8bb8128696
mukoyama-tatsuki <114969055+mukoyama-tatsuki@users.noreply.github.com>
2022-10-27 00:11:23 +0900

    section4-1,4-2,4-q1,4-q2

--- section4-1/sketch.js
+++ section4-1/sketch.js
@@ -1,50 +1,28 @@
-// テキスト「配列」～「配列を使った描画」までを収録
 function setup(){
   createCanvas(400, 400);
   background(240);
-
-  // 配列をランダムに初期化する
-  let scores = [];
-  for(let i = 0; i < 10; i++){
-    scores[i] = random(20, 100); // 60以上100未満のランダムな数を代入
+  for(let i=0;i<10;i++){
+    line(0,i*40,400,i*40);
   }
-  console.log(scores);
 
-  // 合計を計算する
+  // 点数を乱数で用意する
+  let scores = [];
   let sum = 0;
-  for(let i = 0; i < scores.length; i++){
+  for(let i = 0; i < 10; i++){
+    scores[i] = random(60, 100); // 60以上100未満のランダムな数を代入
+    scores[i] = Math.floor(scores[i]);
     sum += scores[i];
   }
-  console.log(sum);
+ 
 
-  // ここから平均・最大・最小を求めます
-  let average, largest, smallest;
-  // BLANK[1]　平均値（ヒント average = 合計 / 配列の長さ）
-
-  largest = 0;
+  // 棒を描く
   for(let i = 0; i < scores.length; i++){
-    // BLANK[2]　ヒント：今までの最大値 largest と scores[i] を比較する
+    const dx = width / scores.length;   // 棒の幅（固定）
+    const h = height * scores[i] / 100; // 棒の高さ（点数に比例）
+    if(scores[i]>=95){fill (255,0,0);}
+    else if(scores[i]<=65){fill(0,0,255);}
+    else{fill(128)};
+    noStroke();
+    rect(1+i * dx, height - h, dx-3, h);
   }
-
-  smallest = 100;
-  for(let i = 0; i < scores.length; i++){
-    // BLANK[3]　ヒント：最小値とだいたい同じ
-  }
-
-  // ここから棒グラフを描いていきます。まずは背景に横線をn本引く
-  const n = 10;
-  for(let i = 0; i < n; i++){ line(0, height * i / n, width, height * i / n); }
-  
-  noStroke();
-
-  for(let i = 0; i < scores.length; i++){
-    const dx = width / scores.length;
-    const h = height * scores[i] / 100;
-    // BLANK[4] ヒント: 条件分岐を使って色を変更します
-    rect(i * dx + 2, height - h, dx - 4, h);
-    fill(0);
-    text(scores[i].toPrecision(3), i * dx, height - h);
-  }
-
-  // BLANK[5] 平均点の線を引きます
 }
