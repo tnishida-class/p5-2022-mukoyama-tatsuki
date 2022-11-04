06021caabc77ba114a528fc757bae732d75e0e3b
mukoyama-tatsuki <114969055+mukoyama-tatsuki@users.noreply.github.com>
2022-11-04 16:52:20 +0900

    section5-1,5-2,5-q1

--- section4-1/sketch.js
+++ section4-1/sketch.js
@@ -14,15 +14,24 @@ function setup(){
     sum += scores[i];
   }
  
-
   // 棒を描く
   for(let i = 0; i < scores.length; i++){
     const dx = width / scores.length;   // 棒の幅（固定）
     const h = height * scores[i] / 100; // 棒の高さ（点数に比例）
+    let largest = scores[0];
+   
     if(scores[i]>=95){fill (255,0,0);}
     else if(scores[i]<=65){fill(0,0,255);}
     else{fill(128)};
     noStroke();
     rect(1+i * dx, height - h, dx-3, h);
+    fill(0);
+    text(scores[i],1+i*dx,height-h);
   }
+
+  let average =sum/10;
+  fill(0);
+  text(average,0,height-average*4);
+  stroke(0,209,13);
+  line(0,height-average*4,400,height-average*4);
 }
